f77765368a1519d7ed2c7cb4da6b827c6dcc1676
Rob Moss <robm.dev@gmail.com>
2017-02-25 22:23:38 +1100

    misc: post about our board game collection

---
+++ _posts/2017-02-25-Fledgling-board-game-collection.md
@@ -0,0 +1,90 @@
+---
+layout: post
+title: Fledgling board game collection
+category: misc
+tags: ['board games']
+extra_js:
+  - jquery-3.1.1.min.js
+  - jquery.imagemapster.min.js
+extra_css:
+  - bgg.css
+---
+
+As a child, Scrabble was our family's board game of choice.
+We were all well-read and had a sufficiently diverse vocabulary that games
+could go down to the wire despite substantial differences in age.
+Monopoly was a painful and frustrating experience, replayed so infrequently
+that the memories were dulled by time and the agony was rediscovered all over
+again (I've since learnt that this may have been due to an all-too-common
+relaxation of the official rules, but that is scant comfort for the hours
+lost).
+Snakes and Ladders, stripped of its karmic lessons, was unbearably nihilistic. In my adolescence I became (loosely) aware to tabletop wargames and Dungeons &
+Dragons, but primarily through video games that borrowed from these genres.
+Through my own failings, it took a relocation to Paris in my late twenties to
+become exposed to the much richer variety of board games that are available,
+populating entire bookshelves in friends' apartments and the forming the
+primary focus of entire afternoons and evenings.
+They even made frequent appearances during working hours, thanks to the
+seriousness with which the French treat the lunch break.
+Julien, Thibault, and colleagues, you know who you are!
+
+Having remained in the same country (heck, the same city!)
+for more two years now, we've begun enjoying minor luxuries such as buying the
+occasional book (much too heavy and cumbersome to accumulate when relocating
+to a new continent every other year).
+And some of our closest friends (figuratively *and* literally) have a keen
+interest in board games, with shelves groaning under the weight (be it
+metaphoric or, indeed, physical) of numerous games.
+Partly out of a guilt stimulated by no such abundance on our part, but
+predominantly out of a desire to sate our own appetites, we are slowly
+addressing this imbalance.
+For now, the carpeted floor serves as a shelf of sorts to this, our fledgling
+board game collection ...
+
+<noscript>
+Please enable JavaScript and reload this page.
+</noscript>
+
+<div class="bgg">
+  <div class="map">
+    <map name="map">
+      <area shape="poly" coords="40,210,190,208,200,550,50,550"
+            href="#" id="172081"
+            class="map-area" />
+      <area shape="poly" coords="188,175,277,175,290,550,200,550"
+            href="#" id="163412"
+            class="map-area" />
+      <area shape="poly" coords="280,155,355,155,365,550,290,550"
+            href="#" id="131835"
+            class="map-area" />
+      <area shape="poly" coords="350,45,440,45,445,550,365,550"
+            href="#" id="320"
+            class="map-area" />
+      <area shape="poly" coords="440,35,575,35,570,558,445,555"
+            href="#" id="822"
+            class="map-area" />
+      <area shape="poly" coords="575,35,685,35,680,550,570,545"
+            href="#" id="39856"
+            class="map-area" />
+      <area shape="poly" coords="685,35,840,40,825,550,680,550"
+            href="#" id="155821"
+            class="map-area" />
+      <area shape="poly"
+            coords="845,2,990,2,955,550,825,550,840,40,800,40"
+            href="#" id="42215"
+            class="map-area" />
+    </map>
+    {% capture img %}{{ page.date | date: "%Y-%m-%d-collection.jpg" }}{% endcapture %}
+    <img usemap="#map" id="img-map" src="{{ site.baseurl }}{{ site.url_img }}/{{ img }}" alt="">
+  </div>
+</div>
+
+<script type="text/javascript" src="{{ site.baseurl }}{{ site.url_js }}/bgg.js"></script>
+<script type="text/javascript">
+    BGG.load({
+        img_map: "#img-map",
+        username: "2bago",
+        userid: 1451771,
+        show_on_hover: false,
+    });
+</script>
---
+++ assets/js/bgg.js
@@ -0,0 +1,244 @@
+/**
+ * bgg.js
+ *
+ * Loads details about board game collections from Board Game Geek.
+ *
+ * Copyright 2017 Robert Moss.
+ *
+ * This work is distributed under the terms of the BSD 3-Clause License
+ * (http://opensource.org/licenses/BSD-3-Clause).
+ *
+ * See the Board Game Geek API documentation for details:
+ *
+ *     https://boardgamegeek.com/wiki/page/BGG_XML_API2
+ *
+ * See the following post for an example of using CORS Anywhere to retrieve
+ * data from Board Game Geek:
+ *
+ *     https://boardgamegeek.com/article/18447252#18447252
+ *
+ * This script makes use of jQuery and ImageMapster, both of which are located
+ * in this directory and are distributed under the terms of the MIT License
+ * (see LICENSE.txt).
+ */
+
+// Create a new namespace to contain all of the variables and functions.
+var BGG = BGG || {};
+
+BGG.set_global_tooltip = function(img_map, content) {
+    var areas = document.evaluate(
+        "//map/area", document, null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (var i = 0; i < areas.snapshotLength; i++) {
+        var area = areas.snapshotItem(i);
+        var game_id = area.getAttribute('id');
+        $(img_map).mapster('set_options', {
+            areas: [{
+                key: game_id,
+                toolTip: '<div class="close">' +
+                    '<a class="close theme-red" href="javascript:;">X</a>' +
+                    '</div>' +
+                    content,
+            }]
+        });
+    }
+
+}
+
+BGG.initialised = false;
+
+BGG.init = function(args) {
+    // Default to showing tooltips when an area is clicked, rather than when
+    // the cursor hovers over an area, for better mobile compatibility.
+    if (args.show_on_hover === undefined) {
+        args.show_on_hover = false;
+    }
+    // var tool_tip_close = ['tooltip-click'];
+    var tool_tip_close = [];
+    if (args.show_on_hover) {
+        // Close tooltips when an area (including the one to which the tooltip
+        // belongs) is clicked. This will prevent tooltips being shown when an
+        // area is clicked!
+        tool_tip_close.push('area-click');
+    }
+
+    // Activate ImageMapster.
+    $(args.img_map).mapster({
+        fill: true,
+        fillOpacity: 0.25,
+        fillColor: '000000',
+        stroke: true,
+        strokeOpacity: 0.5,
+        strokeColor: '000000',
+        strokeWidth: 1,
+        showToolTip: args.show_on_hover,
+        toolTipContainer: '<div class="tooltip bgg"></div>',
+        toolTipClose: tool_tip_close,
+        mapKey: 'id',
+        isSelectable: false,
+        isDeselectable: false,
+        onShowToolTip: function(e) {
+            // Track the most recently shown tooltip.
+            BGG.tooltip_key = e.key;
+            // Make the close link hide the tooltip.
+            var to_close = $('.close');
+            for (var i = 0; i < to_close.length; i++) {
+                to_close[i].onclick = function () {
+                    $(args.img_map).mapster('tooltip');
+                };
+            }
+        },
+        onClick: function(e) {
+            if (! args.show_on_hover) {
+                $(args.img_map).mapster('tooltip', e.key);
+            }
+        },
+    });
+
+    // Define the initial tooltip ("Loading ...") for each area.
+    BGG.set_global_tooltip(args.img_map, "<div>Loading ...</div>")
+
+    // Use CORS Anywhere to retrieve data from Board Game Geek.
+    jQuery.ajaxPrefilter(function(options) {
+        if (options.crossDomain && jQuery.support.cors) {
+            if (options.url.indexOf('cors-anywhere.herokuapp.com') != -1) {
+                return;
+            }
+            var http = '';
+            if (window.location.protocol === 'http:') {
+                http = 'http:'
+            } else {
+                // Default to HTTPS.
+                http = 'https:'
+            }
+            options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+            options.origin = 'X-Requested-With';
+            console.log('Using cors-anywhere via ' + http);
+        } else if (options.crossDomain) {
+            console.log('Cannot use cors-anywhere');
+            console.log(options);
+        }
+    });
+
+    BGG.initialised = true;
+}
+
+BGG.error = function(img_map, msg) {
+    BGG.set_global_tooltip(img_map,
+                           '<div class="error">Error: ' + msg + '</div>');
+}
+
+BGG.request = function(url, data, success_fn, img_map) {
+    var query = {
+        method: "GET",
+        dataType : "xml",
+        attempts: 3,
+        url: url,
+        data: data,
+        success: function(xmldoc, textStatus, xhr) {
+            if (xhr.status == 200) {
+                success_fn(xmldoc);
+            } else if (xhr.status == 202) {
+                query.attempts--;
+                if (query.attempts > 0) {
+                    console.log("Will retry in 5 seconds ...");
+                    setTimeout(function(){ $.ajax(query); },
+                               5000);
+                } else {
+                    BGG.error(img_map, 'No more retries, data unavailable');
+                }
+            } else {
+                BGG.error(img_map, textStatus);
+            }
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            if (xhr.status == 0) {
+                BGG.error(img_map, 'Is JavaScript enabled for ' +
+                          'cors-anywhere.herokuapp.com?');
+            } else {
+                BGG.error(img_map, textStatus);
+            }
+        },
+    };
+    return jQuery.ajax(query);
+}
+
+BGG.tooltip = function(user_id, item) {
+    var name = item.getElementsByTagName('name').item(0).textContent;
+    var game_id = item.getAttribute('objectid');
+    var name_href = 'https://boardgamegeek.com/boardgame/' + game_id;
+    var name_link = '<a class="theme-green" href="' + name_href + '">' +
+        name + '</a>';
+    var play_href = 'https://boardgamegeek.com/plays/thing/' + game_id +
+        '?userid=' + user_id;
+    var plays = item.getElementsByTagName('numplays').item(0).textContent;
+    var play_link = '0 plays';
+    if (parseInt(plays) >= 1) {
+        play_link = '<a class="theme-blue" href="' + play_href + '">' +
+            plays + ' plays</a>';
+    }
+    var comm = item.getElementsByTagName('comment').item(0).textContent;
+    var rating = item.getElementsByTagName('rating').item(0).getAttribute('value');
+    if (rating === 'N/A') {
+        rating = 'Not rated';
+    } else {
+        rating = rating + ' / 10';
+    }
+    var tool_tip = '<div class="game">' +
+        '<div class="close">' +
+        '<a class="close theme-red" href="javascript:;">X</a>' +
+        '</div>' +
+        '<div class="name theme-green">' + name_link + '</div>' +
+        '<div class="rating theme-yellow">' + rating + '</div>' +
+        '<div class="plays theme-blue">' + play_link + '</div>' +
+        '<div class="comment">' + comm + '</div>' +
+        '</div>';
+    return tool_tip;
+}
+
+BGG.process = function(img_map, user_id, xml_collection) {
+    var items = xml_collection.evaluate(
+        "//item", xml_collection, null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (var i = 0; i < items.snapshotLength; i++) {
+        var item = items.snapshotItem(i);
+        var game_id = item.getAttribute('objectid');
+        $(img_map).mapster('set_options', {
+            areas: [{
+                key: game_id,
+                toolTip: BGG.tooltip(user_id, item),
+            }]
+        });
+    }
+    // Reload the most recent tooltip if it's still visible.
+    var num_tooltips = $('.tooltip').length;
+    if (BGG.tooltip_key !== undefined && num_tooltips === 1) {
+        // Note: the tooltip must first be deactivated, in order to have its
+        // contents updated.
+        $(img_map).mapster('tooltip');
+        $(img_map).mapster('tooltip', BGG.tooltip_key);
+    }
+}
+
+BGG.load = function(args) {
+    var defaults = {
+        collection_url: "https://www.boardgamegeek.com/xmlapi2/collection",
+        show_on_hover: false,
+    };
+    args = $.extend({}, defaults, args);
+    if (! BGG.initialised) {
+        BGG.init(args);
+    }
+    // Note: if we need to request multiple documents, save each document in
+    // the BGG object and call BGG.process() when all documents are defined.
+    BGG.request(args.collection_url,
+                { username: args.username,
+                  own: 1,
+                  stats: 1,
+                  excludesubtype: 'boardgameexpansion',
+                },
+                function(xmldoc) {
+                    BGG.process(args.img_map, args.userid, xmldoc);
+                },
+                args.img_map);
+}
